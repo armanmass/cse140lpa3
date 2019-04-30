77c9bbe7322b6058a15d4759c03febce54104f27
Bryan Chin <b5chin@eng.ucsd.edu>
2019-04-29 19:55:13 -0700

    added b7 input

--- dispString.v
+++ dispString.v
@@ -46,6 +46,7 @@ module dispString(
 		  input wire [7:0] b4,
 		  input wire [7:0] b5,
 		  input wire [7:0] b6,
+		  input wire [7:0] b7,
 		  input wire go,
 		  input wire 	   rst,
 		  input wire 	   clk);
@@ -66,13 +67,14 @@ module dispString(
    end
 
    assign dOutP = 
-		 (cnt[3:1]==3'b000) ? b0 :
-		 (cnt[3:1]==3'b001) ? b1 :
-		 (cnt[3:1]==3'b010) ? b2 :
-		 (cnt[3:1]==3'b011) ? b3 :
-		 (cnt[3:1]==3'b100) ? b4 :
-		 (cnt[3:1]==3'b101) ? b5 :
-		 (cnt[3:1]==3'b110) ? b6 : 8'h0d;
+		  (cnt[3:1]==3'b000) ? b0 :
+		  (cnt[3:1]==3'b001) ? b1 :
+		  (cnt[3:1]==3'b010) ? b2 :
+		  (cnt[3:1]==3'b011) ? b3 :
+		  (cnt[3:1]==3'b100) ? b4 :
+		  (cnt[3:1]==3'b101) ? b5 :
+		  (cnt[3:1]==3'b110) ? b6 : b7;
+   
    
    
 endmodule // dispString
